791f619e8bf5bb45b4e36dc75cf9d11849d3932a
Tim Razumov <timofey.razumov@corp.mail.ru>
2020-03-19 23:59:51 +0300

    add memcache

---
+++ app/session/repository/memCache.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"errors"
+	"sync"
+
+	//"github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/models"
+	"github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/session"
+)
+
+type SessionStore struct {
+	sessions map[string]uint
+	mu       *sync.Mutex
+}
+
+func CreateRepository() session.Repository {
+	return &SessionStore{
+		sessions: make(map[string]uint),
+		mu:       &sync.Mutex{},
+	}
+}
+
+func (sessionStore *SessionStore) Create(id uint) (string, error) {
+	sessionStore.mu.Lock()
+	defer sessionStore.mu.Unlock()
+	// TODO: норм хэширование
+	sid := "session_id"
+	sessionStore.sessions[sid] = id
+	return sid, nil
+}
+
+func (sessionStore *SessionStore) Get(sid string) (uint, bool) {
+	sessionStore.mu.Lock()
+	defer sessionStore.mu.Unlock()
+	id, has := sessionStore.sessions[sid]
+	return id, has
+}
+
+func (sessionStore *SessionStore) Delete(sid string) (err error) {
+	sessionStore.mu.Lock()
+	defer sessionStore.mu.Unlock()
+	if _, has := sessionStore.sessions[sid]; has {
+		delete(sessionStore.sessions, sid)
+	} else {
+		err = errors.New("no key")
+	}
+	return err
+}
