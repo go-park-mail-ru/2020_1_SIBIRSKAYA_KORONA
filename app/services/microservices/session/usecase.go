c05814e3878b9bacc989c19d9b6f1a236cc0d009
Anton Chetverov <678summerdays@gmail.com>
2020-04-28 21:22:09 +0300

    Fix in mocks

--- app/services/microservices/session/usecase.go
+++ app/services/microservices/session/usecase.go
@@ -2,7 +2,7 @@ package session
 
 import "github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/models"
 
-//go:generate mockgen -source=usecase.go -package=mocks -destination=./mocks/user_usecase_mock.go
+//go:generate mockgen -source=usecase.go -package=mocks -destination=./mocks/session_usecase_mock.go
 type UseCase interface {
 	Create(sess models.Session) error
 	Get(sid string) (uint, error)
