c6f867628f3a32f5324f6475372f1bd779c4fbf5
Anton Chetverov <678summerdays@gmail.com>
2020-04-30 18:17:22 +0300

    Repair in tests

--- cmd/session/main.go
+++ cmd/session/main.go
@@ -15,12 +15,10 @@ var opts struct {
 	configPath string
 }
 
-func init() {
+func main() {
 	flag.StringVar(&opts.configPath, "c", "", "path to configuration file")
 	flag.StringVar(&opts.configPath, "config", "", "path to configuration file")
-}
 
-func main() {
 	flag.Parse()
 	viper.SetConfigFile(opts.configPath)
 	err := viper.ReadInConfig()
