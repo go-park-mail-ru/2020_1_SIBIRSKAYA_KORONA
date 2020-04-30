f79fcd566c41316e3c675e3837cf18bf244187d4
Tim Razumov <timofey.razumov@corp.mail.ru>
2020-04-30 19:24:23 +0300

    board use case (93%)

--- app/services/api/board/usecase/usecase_test.go
+++ app/services/api/board/usecase/usecase_test.go
@@ -97,6 +97,27 @@ func TestGet(t *testing.T) {
 	assert.Equal(t, board, testBoard)*/
 }
 
+func TestGetBoardsByUser(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	userRepoMock, boardRepoMock := createRepoMocks(ctrl)
+	bUsecase := boardUseCase.CreateUseCase(userRepoMock, boardRepoMock)
+
+	var id uint = 1
+
+	// good
+	boardRepoMock.EXPECT().GetBoardsByUser(id).Return(nil, nil, nil)
+	_, _, err := bUsecase.GetBoardsByUser(id)
+	assert.Nil(t, err)
+
+	// error
+	id++
+	boardRepoMock.EXPECT().GetBoardsByUser(id).Return(nil, nil, errors.ErrBoardNotFound)
+	_, _, err = bUsecase.GetBoardsByUser(id)
+	assert.Equal(t, err, errors.ErrBoardNotFound)
+}
+
 func TestDelete(t *testing.T) {
 	t.Parallel()
 	ctrl := gomock.NewController(t)
@@ -108,20 +129,22 @@ func TestDelete(t *testing.T) {
 	err := faker.FakeData(&testBoard)
 	assert.NoError(t, err)
 
+	// good
 	boardRepoMock.EXPECT().Delete(testBoard.ID).Return(nil)
-
 	err = bUsecase.Delete(testBoard.ID)
 	assert.NoError(t, err)
-}
 
-/*
+	// error
+	testBoard.ID++
+	boardRepoMock.EXPECT().Delete(testBoard.ID).Return(errors.ErrUserNotFound)
+	err = bUsecase.Delete(testBoard.ID)
+	assert.Equal(t, err, errors.ErrUserNotFound)
+}
 
 func TestGetColumnsByID(t *testing.T) {
 	t.Parallel()
-
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-
 	userRepoMock, boardRepoMock := createRepoMocks(ctrl)
 	bUsecase := boardUseCase.CreateUseCase(userRepoMock, boardRepoMock)
 
@@ -129,21 +152,45 @@ func TestGetColumnsByID(t *testing.T) {
 	err := faker.FakeData(&testBoard)
 	assert.NoError(t, err)
 
-	boardRepoMock.EXPECT().
-		GetColumnsByID(testBoard.ID).
-		Return(nil, nil)
-
+	// good
+	boardRepoMock.EXPECT().GetColumnsByID(testBoard.ID).Return(nil, nil)
 	column, err := bUsecase.GetColumnsByID(testBoard.ID)
 	assert.Nil(t, column)
 	assert.NoError(t, err)
+
+	// error
+	boardRepoMock.EXPECT().GetColumnsByID(testBoard.ID).Return(nil, errors.ErrColNotFound)
+	column, err = bUsecase.GetColumnsByID(testBoard.ID)
+	assert.Nil(t, column)
+	assert.Equal(t, err, errors.ErrColNotFound)
+}
+
+func TestGetLabelsByID(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	userRepoMock, boardRepoMock := createRepoMocks(ctrl)
+	bUsecase := boardUseCase.CreateUseCase(userRepoMock, boardRepoMock)
+
+	var bid uint = 1
+
+	// good
+	boardRepoMock.EXPECT().GetLabelsByID(bid).Return(nil, nil)
+	column, err := bUsecase.GetLabelsByID(bid)
+	assert.Nil(t, column)
+	assert.NoError(t, err)
+
+	// error
+	boardRepoMock.EXPECT().GetLabelsByID(bid).Return(nil, errors.ErrLabelNotFound)
+	column, err = bUsecase.GetLabelsByID(bid)
+	assert.Nil(t, column)
+	assert.Equal(t, err, errors.ErrLabelNotFound)
 }
 
 func TestUpdate(t *testing.T) {
 	t.Parallel()
-
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-
 	userRepoMock, boardRepoMock := createRepoMocks(ctrl)
 	bUsecase := boardUseCase.CreateUseCase(userRepoMock, boardRepoMock)
 
@@ -151,11 +198,103 @@ func TestUpdate(t *testing.T) {
 	err := faker.FakeData(&testBoard)
 	assert.NoError(t, err)
 
-	boardRepoMock.EXPECT().
-		Update(&testBoard).
-		Return(nil)
-
+	// good
+	boardRepoMock.EXPECT().Update(&testBoard).Return(nil)
 	err = bUsecase.Update(&testBoard)
 	assert.NoError(t, err)
+
+	// error
+	testBoard.ID++
+	boardRepoMock.EXPECT().Update(&testBoard).Return(errors.ErrBoardNotFound)
+	err = bUsecase.Update(&testBoard)
+	assert.Equal(t, err, errors.ErrBoardNotFound)
 }
-*/
+
+func TestGetUsersForInvite(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	userRepoMock, boardRepoMock := createRepoMocks(ctrl)
+	bUsecase := boardUseCase.CreateUseCase(userRepoMock, boardRepoMock)
+
+	var bid uint = 0
+	var limit uint = 0
+	nicknamePart := "aaa"
+
+	// good
+	boardRepoMock.EXPECT().GetUsersForInvite(bid, nicknamePart, limit).Return(nil, nil)
+	_, err := bUsecase.GetUsersForInvite(bid, nicknamePart, limit)
+	assert.NoError(t, err)
+
+	// error
+	bid++
+	boardRepoMock.EXPECT().GetUsersForInvite(bid, nicknamePart, limit).Return(nil, errors.ErrUserNotFound)
+	_, err = bUsecase.GetUsersForInvite(bid, nicknamePart, limit)
+	assert.Equal(t, err, errors.ErrUserNotFound)
+}
+
+func TestInviteMember(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	userRepoMock, boardRepoMock := createRepoMocks(ctrl)
+	bUsecase := boardUseCase.CreateUseCase(userRepoMock, boardRepoMock)
+
+	var testUser models.User
+	err := faker.FakeData(&testUser)
+	assert.NoError(t, err)
+	var bid uint
+
+	// good
+	userRepoMock.EXPECT().GetByID(testUser.ID).Return(&testUser, nil)
+	boardRepoMock.EXPECT().InviteMember(bid, &testUser).Return(nil)
+	err = bUsecase.InviteMember(bid, testUser.ID)
+	assert.NoError(t, err)
+
+	// error user not found
+	testUser.ID++
+	userRepoMock.EXPECT().GetByID(testUser.ID).Return(nil, errors.ErrUserNotFound)
+	err = bUsecase.InviteMember(bid, testUser.ID)
+	assert.Equal(t, err, errors.ErrUserNotFound)
+
+	// board not found
+	testUser.ID--
+	bid++
+	userRepoMock.EXPECT().GetByID(testUser.ID).Return(&testUser, nil)
+	boardRepoMock.EXPECT().InviteMember(bid, &testUser).Return(errors.ErrBoardNotFound)
+	err = bUsecase.InviteMember(bid, testUser.ID)
+	assert.Equal(t, err, errors.ErrBoardNotFound)
+}
+
+func TestDeleteMember(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	userRepoMock, boardRepoMock := createRepoMocks(ctrl)
+	bUsecase := boardUseCase.CreateUseCase(userRepoMock, boardRepoMock)
+
+	var testUser models.User
+	err := faker.FakeData(&testUser)
+	assert.NoError(t, err)
+	var bid uint
+
+	// good
+	userRepoMock.EXPECT().GetByID(testUser.ID).Return(&testUser, nil)
+	boardRepoMock.EXPECT().DeleteMember(bid, &testUser).Return(nil)
+	err = bUsecase.DeleteMember(bid, testUser.ID)
+	assert.NoError(t, err)
+
+	// error user not found
+	testUser.ID++
+	userRepoMock.EXPECT().GetByID(testUser.ID).Return(nil, errors.ErrUserNotFound)
+	err = bUsecase.DeleteMember(bid, testUser.ID)
+	assert.Equal(t, err, errors.ErrUserNotFound)
+
+	// board not found
+	testUser.ID--
+	bid++
+	userRepoMock.EXPECT().GetByID(testUser.ID).Return(&testUser, nil)
+	boardRepoMock.EXPECT().DeleteMember(bid, &testUser).Return(errors.ErrBoardNotFound)
+	err = bUsecase.DeleteMember(bid, testUser.ID)
+	assert.Equal(t, err, errors.ErrBoardNotFound)
+}
