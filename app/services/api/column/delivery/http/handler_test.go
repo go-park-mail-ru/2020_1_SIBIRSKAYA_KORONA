33e76b5a359d95cf126df7c54ae76bd55fc59424
Anton Chetverov <678summerdays@gmail.com>
2020-05-21 17:04:21 +0300

    Column handler tests

--- app/services/api/column/delivery/http/handler_test.go
+++ app/services/api/column/delivery/http/handler_test.go
@@ -30,3 +30,121 @@ func TestCreateHandler(t *testing.T) {
 		nil, nil, nil, nil, nil, nil)
 	columnHandler.CreateHandler(router, columnUsecaseMock, mw)
 }
+
+// func TestCreate(t *testing.T) {
+// 	// t.Skip()
+// 	t.Parallel()
+
+// 	ctrl := gomock.NewController(t)
+// 	defer ctrl.Finish()
+
+// 	checklistUsecaseMock := checklistMocks.NewMockUseCase(ctrl)
+// 	handler := checklistHandler.ChecklistHandler{UseCase: checklistUsecaseMock}
+
+// 	var testColumn models.Column
+// 	err := faker.FakeData(&testColumn)
+// 	assert.NoError(t, err)
+
+// 	var testBoard models.Board
+// 	err = faker.FakeData(&testBoard)
+// 	assert.NoError(t, err)
+
+// 	body, err := testColumn.MarshalJSON()
+// 	assert.NoError(t, err)
+
+// 	{
+// 		router := echo.New()
+// 		request := test.NewRequest(echo.POST, "/", strings.NewReader(""))
+// 		response := test.NewRecorder()
+// 		context := router.NewContext(request, response)
+// 		context.Set("body", body)
+// 		context.Set("bid", testColumn.ID)
+
+// 		checklistUsecaseMock.EXPECT().
+// 			Create(gomock.Any()).
+// 			Return(nil)
+
+// 		err = handler.Create(context)
+
+// 		assert.NoError(t, err)
+// 		assert.Equal(t, context.Response().Status, http.StatusOK)
+// 	}
+
+// 	{
+// 		router := echo.New()
+// 		request := test.NewRequest(echo.POST, "/", strings.NewReader(""))
+// 		response := test.NewRecorder()
+// 		context := router.NewContext(request, response)
+// 		context.Set("body", body)
+// 		context.Set("tid", testTask.ID)
+
+// 		checklistUsecaseMock.EXPECT().
+// 			Create(gomock.Any()).
+// 			Return(errors.ErrDbBadOperation)
+
+// 		err = handler.Create(context)
+// 		if err != nil {
+// 			t.Error(err)
+// 		}
+// 		assert.Equal(t, context.Response().Status, http.StatusInternalServerError)
+// 	}
+
+// }
+
+// func TestGet(t *testing.T) {
+// 	// t.Skip()
+// 	t.Parallel()
+
+// 	ctrl := gomock.NewController(t)
+// 	defer ctrl.Finish()
+
+// 	checklistUsecaseMock := checklistMocks.NewMockUseCase(ctrl)
+// 	handler := checklistHandler.ChecklistHandler{UseCase: checklistUsecaseMock}
+
+// 	var testChecklist models.Checklist
+// 	err := faker.FakeData(&testChecklist)
+// 	assert.NoError(t, err)
+
+// 	var testTask models.Task
+// 	err = faker.FakeData(&testTask)
+// 	assert.NoError(t, err)
+
+// 	{
+// 		router := echo.New()
+// 		request := test.NewRequest(echo.POST, "/", strings.NewReader(""))
+// 		response := test.NewRecorder()
+// 		context := router.NewContext(request, response)
+// 		context.Set("tid", testTask.ID)
+
+// 		checklistUsecaseMock.EXPECT().
+// 			Get(testTask.ID).
+// 			Return(nil, errors.ErrDbBadOperation)
+
+// 		err = handler.Get(context)
+// 		if err != nil {
+// 			t.Error(err)
+// 		}
+// 		assert.Equal(t, context.Response().Status, http.StatusInternalServerError)
+// 	}
+
+// 	var checklists models.Checklists
+// 	checklists = append(checklists, testChecklist)
+// 	{
+// 		router := echo.New()
+// 		request := test.NewRequest(echo.POST, "/", strings.NewReader(""))
+// 		response := test.NewRecorder()
+// 		context := router.NewContext(request, response)
+// 		context.Set("tid", testTask.ID)
+
+// 		checklistUsecaseMock.EXPECT().
+// 			Get(testTask.ID).
+// 			Return(checklists, nil)
+
+// 		err = handler.Get(context)
+// 		if err != nil {
+// 			t.Error(err)
+// 		}
+// 		assert.Equal(t, context.Response().Status, http.StatusOK)
+// 	}
+
+// }
