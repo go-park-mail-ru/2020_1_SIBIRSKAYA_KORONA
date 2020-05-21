12d57bf01ae6e3f1269c0fd830102be53fdb585a
Тимофей Разумов <timofey.razumov@corp.mail.ru>
2020-05-21 17:02:23 +0300

    notification test

--- app/services/api/notification/delivery/http/handler_test.go
+++ app/services/api/notification/delivery/http/handler_test.go
@@ -1,1 +1,133 @@
 package http_test
+
+import (
+	"net/http"
+	test "net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/models"
+	drelloMiddleware "github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/services/api/middleware"
+	ntfHandler "github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/services/api/notification/delivery/http"
+	ntfMocks "github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/services/api/notification/mocks"
+
+	"github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/pkg/errors"
+	"github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/pkg/logger"
+
+	"github.com/bxcodec/faker"
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	logger.InitLogger()
+	os.Exit(m.Run())
+}
+
+func TestCreateHandler(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	ntfUseCaseMock := ntfMocks.NewMockUseCase(ctrl)
+
+	router := echo.New()
+	mw := drelloMiddleware.CreateMiddleware(nil, nil,nil,nil,nil,
+		nil,nil,nil,nil, nil,nil)
+	ntfHandler.CreateHandler(router, ntfUseCaseMock, mw)
+}
+
+func TestGetAll(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	htfUseCaseMock := ntfMocks.NewMockUseCase(ctrl)
+	handler := ntfHandler.NotificationHandler{UseCase: htfUseCaseMock}
+
+	var testEvents models.Events
+	err := faker.FakeData(&testEvents)
+	assert.NoError(t, err)
+
+	// good
+	{
+		var uid uint = 10
+		ctx := echo.New().NewContext(nil, test.NewRecorder())
+		htfUseCaseMock.EXPECT().GetAll(uid).Return(testEvents, true)
+		ctx.Set("uid", uid)
+		err = handler.GetAll(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, ctx.Response().Status, http.StatusOK)
+	}
+
+	// error
+	{
+		var uid uint = 10
+		ctx := echo.New().NewContext(nil, test.NewRecorder())
+		htfUseCaseMock.EXPECT().GetAll(uid).Return(testEvents, false)
+		ctx.Set("uid", uid)
+		err = handler.GetAll(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, ctx.Response().Status, http.StatusOK)
+	}
+
+}
+
+func TestUpdateAll(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	htfUseCaseMock := ntfMocks.NewMockUseCase(ctrl)
+	handler := ntfHandler.NotificationHandler{UseCase: htfUseCaseMock}
+
+	// good
+	{
+		var uid uint = 10
+		ctx := echo.New().NewContext(nil, test.NewRecorder())
+		htfUseCaseMock.EXPECT().UpdateAll(uid).Return(nil)
+		ctx.Set("uid", uid)
+		err := handler.UpdateAll(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, ctx.Response().Status, http.StatusOK)
+	}
+
+	// error
+	{
+		var uid uint = 10
+		ctx := echo.New().NewContext(nil, test.NewRecorder())
+		htfUseCaseMock.EXPECT().UpdateAll(uid).Return(errors.ErrDbBadOperation)
+		ctx.Set("uid", uid)
+		err := handler.UpdateAll(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, ctx.Response().Status, http.StatusInternalServerError)
+	}
+}
+
+func TestDeleteAll(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	htfUseCaseMock := ntfMocks.NewMockUseCase(ctrl)
+	handler := ntfHandler.NotificationHandler{UseCase: htfUseCaseMock}
+
+	// good
+	{
+		var uid uint = 10
+		ctx := echo.New().NewContext(nil, test.NewRecorder())
+		htfUseCaseMock.EXPECT().DeleteAll(uid).Return(nil)
+		ctx.Set("uid", uid)
+		err := handler.DeleteAll(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, ctx.Response().Status, http.StatusOK)
+	}
+
+	// error
+	{
+		var uid uint = 10
+		ctx := echo.New().NewContext(nil, test.NewRecorder())
+		htfUseCaseMock.EXPECT().DeleteAll(uid).Return(errors.ErrDbBadOperation)
+		ctx.Set("uid", uid)
+		err := handler.DeleteAll(ctx)
+		assert.NoError(t, err)
+		assert.Equal(t, ctx.Response().Status, http.StatusInternalServerError)
+	}
+}
--- app/services/api/user/delivery/http/handler_test.go
+++ app/services/api/user/delivery/http/handler_test.go
@@ -2,8 +2,6 @@ package http_test
 
 import (
 	"bytes"
-	"github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/pkg/errors"
-	"github.com/labstack/echo/v4"
 	"log"
 	"mime/multipart"
 	"net/http"
@@ -13,12 +11,16 @@ import (
 	"testing"
 
 	"github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/models"
+	drelloMiddleware "github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/services/api/middleware"
 	userHandler "github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/services/api/user/delivery/http"
 	userMocks "github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/services/api/user/mocks"
+
+	"github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/pkg/errors"
 	"github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/pkg/logger"
 
 	"github.com/bxcodec/faker"
 	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -36,6 +38,18 @@ func GetContexFromJSON(method, path string) echo.Context {
 	request := test.NewRequest(method, path, body)
 	return echo.New().NewContext(request, test.NewRecorder())
 }*/
+
+func TestCreateHandler(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	userUseCaseMock := userMocks.NewMockUseCase(ctrl)
+
+	router := echo.New()
+	mw := drelloMiddleware.CreateMiddleware(nil, nil,nil,nil,nil,
+		nil,nil,nil,nil, nil,nil)
+	userHandler.CreateHandler(router, userUseCaseMock, mw)
+}
 
 func TestCreate(t *testing.T) {
 	t.Parallel()
