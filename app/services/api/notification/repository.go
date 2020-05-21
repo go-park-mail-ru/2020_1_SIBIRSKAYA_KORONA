ab436dc8bc8f3a0817dad249893673905c0475ae
Anton Chetverov <678summerdays@gmail.com>
2020-05-21 13:38:54 +0300

    Generate mocks for notification

--- app/services/api/notification/repository.go
+++ app/services/api/notification/repository.go
@@ -2,6 +2,7 @@ package notification
 
 import "github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/app/models"
 
+//go:generate mockgen -source=repository.go -package=mocks -destination=./mocks/notification_repo_mock.go
 type Repository interface {
 	Create(event *models.Event) error
 	GetAll(uid uint) (models.Events, bool)
