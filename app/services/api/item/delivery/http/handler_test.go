70421915f08c34e0624519805421fe27b7a2e470
Anton Chetverov <678summerdays@gmail.com>
2020-05-21 11:46:08 +0300

    Update item handler tests

--- app/services/api/item/delivery/http/handler_test.go
+++ app/services/api/item/delivery/http/handler_test.go
@@ -6,6 +6,7 @@ import (
 
 	"net/http"
 
+	"github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/pkg/errors"
 	"github.com/go-park-mail-ru/2020_1_SIBIRSKAYA_KORONA/pkg/logger"
 	"go.uber.org/zap/zapcore"
 
@@ -50,22 +51,34 @@ func TestCreate(t *testing.T) {
 	body, err := testItem.MarshalJSON()
 	assert.NoError(t, err)
 
-	router := echo.New()
+	{
+		router := echo.New()
+		request := test.NewRequest(echo.POST, "/", strings.NewReader(""))
+		response := test.NewRecorder()
+		context := router.NewContext(request, response)
+		context.Set("body", body)
+		context.Set("clid", testChecklist.ID)
+		itemUsecaseMock.EXPECT().
+			Create(gomock.Any()).
+			Return(nil)
+		err = handler.Create(context)
+		assert.NoError(t, err)
+		assert.Equal(t, context.Response().Status, http.StatusOK)
+	}
 
-	request := test.NewRequest(echo.POST, "/", strings.NewReader(""))
-	response := test.NewRecorder()
-	context := router.NewContext(request, response)
-	context.Set("body", body)
-	context.Set("clid", testChecklist.ID)
-
-	itemUsecaseMock.EXPECT().
-		Create(gomock.Any()).
-		Return(nil)
-
-	err = handler.Create(context)
-
-	assert.NoError(t, err)
-	assert.Equal(t, context.Response().Status, http.StatusOK)
+	{
+		router := echo.New()
+		request := test.NewRequest(echo.POST, "/", strings.NewReader(""))
+		response := test.NewRecorder()
+		context := router.NewContext(request, response)
+		context.Set("body", body)
+		context.Set("clid", testChecklist.ID)
+		itemUsecaseMock.EXPECT().
+			Create(gomock.Any()).
+			Return(errors.ErrDbBadOperation)
+		err = handler.Create(context)
+		assert.Equal(t, context.Response().Status, http.StatusInternalServerError)
+	}
 }
 
 func TestUpdate(t *testing.T) {
@@ -89,22 +102,66 @@ func TestUpdate(t *testing.T) {
 	body, err := testItem.MarshalJSON()
 	assert.NoError(t, err)
 
-	router := echo.New()
+	{
+		router := echo.New()
+		request := test.NewRequest(echo.POST, "/", strings.NewReader(""))
+		response := test.NewRecorder()
+		context := router.NewContext(request, response)
+		context.Set("body", body)
+		context.Set("itid", testItem.ID)
+		context.Set("clid", testChecklist.ID)
+		itemUsecaseMock.EXPECT().
+			Update(gomock.Any()).
+			Return(nil)
+		err = handler.Update(context)
+		assert.NoError(t, err)
+		assert.Equal(t, context.Response().Status, http.StatusOK)
+	}
 
-	request := test.NewRequest(echo.POST, "/", strings.NewReader(""))
-	response := test.NewRecorder()
-	context := router.NewContext(request, response)
+	{
+		router := echo.New()
+		request := test.NewRequest(echo.POST, "/", strings.NewReader(""))
+		response := test.NewRecorder()
+		context := router.NewContext(request, response)
+		context.Set("body", body)
+		context.Set("itid", testItem.ID)
+		context.Set("clid", testChecklist.ID)
+		itemUsecaseMock.EXPECT().
+			Update(gomock.Any()).
+			Return(errors.ErrDbBadOperation)
+		err = handler.Update(context)
+		assert.Equal(t, context.Response().Status, http.StatusInternalServerError)
+	}
 
-	context.Set("body", body)
-	context.Set("itid", testItem.ID)
-	context.Set("clid", testChecklist.ID)
+}
 
-	itemUsecaseMock.EXPECT().
-		Update(gomock.Any()).
-		Return(nil)
+func TestDelete(t *testing.T) {
+	//t.Skip()
+	t.Parallel()
 
-	err = handler.Update(context)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
+	itemUsecaseMock := itemMocks.NewMockUseCase(ctrl)
+	handler := itemHandler.ItemHandler{UseCase: itemUsecaseMock}
+
+	var testChecklist models.Checklist
+	err := faker.FakeData(&testChecklist)
 	assert.NoError(t, err)
-	assert.Equal(t, context.Response().Status, http.StatusOK)
+
+	var testItem models.Item
+	err = faker.FakeData(&testItem)
+	assert.NoError(t, err)
+
+	{
+		router := echo.New()
+		request := test.NewRequest(echo.POST, "/", strings.NewReader(""))
+		response := test.NewRecorder()
+		context := router.NewContext(request, response)
+
+		context.Set("itid", testItem.ID)
+
+		err = handler.Delete(context)
+		assert.Equal(t, context.Response().Status, http.StatusInternalServerError)
+	}
 }
